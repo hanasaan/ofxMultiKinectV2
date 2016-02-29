0256d38c3563b5802f8ac7fff745579c4e31e517
Matt Felsen <mattfelsen@localprojects.com>
2016-02-29 15:17:42 -0500

    Added addon_config.mk with link to OpenCL framework

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxMultiKinectV2
+	ADDON_DESCRIPTION = Connecting multiple Kinect For Windows V2 sensors to one Mac
+	ADDON_AUTHOR = Yuya Hanai
+	ADDON_TAGS = "kinect" "kinectv2" "libfreenect" "libfreenect2"
+	ADDON_URL = https://github.com/hanasaan/ofxMultiKinectV2
+
+osx:
+	
+	ADDON_FRAMEWORKS = OpenCL
